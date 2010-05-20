a53fe475bc2143ab855a92800418974b63e12a51
Cainã Costa <cainan.costa@gmail.com>
2010-05-20 16:27:55 -0300

    Removing some unnecessary this-> on the hero class

--- src/hero.cpp
+++ src/hero.cpp
@@ -2,7 +2,7 @@
 
 Hero::Hero()
 {
-    this->speed = 0.5;
+    speed = 0.5;
 }
 
 void Hero::move(int move_x, int move_y)
@@ -10,8 +10,8 @@ void Hero::move(int move_x, int move_y)
     // TODO: restrict the boundary of movement.
     float speed_factor = 1 + (this->speed / 10);
 
-    this->x += move_x * speed_factor;
-    this->y += move_y * speed_factor;
+    x += move_x * speed_factor;
+    y += move_y * speed_factor;
 }
 
 void Hero::powerup()
@@ -20,27 +20,27 @@ void Hero::powerup()
 
 void Hero::speedup(float factor)
 {
-    this->speed = this->speed + (this->speed * (factor * 10));
+    speed = speed + (speed * (factor * 10));
 }
 
 int Hero::get_x()
 {
-    return this->x;
+    return x;
 }
 
 int Hero::get_y()
 {
-    return this->y;
+    return y;
 }
 
 int Hero::get_power()
 {
-    return this->power;
+    return power;
 }
 
 int Hero::get_speed()
 {
-    return this->speed;
+    return speed;
 }
 
 void Hero::redraw()
